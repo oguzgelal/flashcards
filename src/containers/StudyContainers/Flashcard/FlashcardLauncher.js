f46fd9f64004cd77b00fc6853fdee31b8adcbf3c
Oguz Gelal <o.gelal77@gmail.com>
2020-01-30 22:58:39 -0500

    launcher dialog now blueprint

--- src/containers/StudyContainers/Flashcard/FlashcardLauncher.js
+++ src/containers/StudyContainers/Flashcard/FlashcardLauncher.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import styled from 'styled-components/macro';
-import get from 'lodash/get';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import styled from "styled-components/macro";
+import get from "lodash/get";
 
-import { Dialog } from 'evergreen-ui';
-import { Card, H5 } from "@blueprintjs/core";
+import { Dialog, Classes, Card, H5, Button } from "@blueprintjs/core";
 
-import FlashcardPreview from '../../../components/StudyComponents/Flashcard/FlashcardPreview';
-import { SESSION_TYPE_FLASHCARD } from '../../../models/SessionFlashcards';
+import FlashcardPreview from "../../../components/StudyComponents/Flashcard/FlashcardPreview";
+import FlashcardSettings, { flashcardSettingsPropTypes } from "./FlashcardSettings";
+import { SESSION_TYPE_FLASHCARD } from "../../../models/SessionFlashcards";
 
-import FlashcardSettings, { flashcardSettingsPropTypes } from './FlashcardSettings';
-
-import * as types from '../../../redux/modules/sessions/types';
-import * as sessionActions from '../../../redux/modules/sessions/actions';
+import * as types from "../../../redux/modules/sessions/types";
+import * as sessionActions from "../../../redux/modules/sessions/actions";
 
 class FlashcardLauncher extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      settings: {},
+      settings: {}
     };
 
     this.start = this.start.bind(this);
@@ -30,7 +28,7 @@ class FlashcardLauncher extends React.Component {
   componentDidMount() {
     this.setState({
       settings: this.props.settings || {}
-    })
+    });
   }
 
   start(callback) {
@@ -38,52 +36,59 @@ class FlashcardLauncher extends React.Component {
       type: SESSION_TYPE_FLASHCARD,
       settings: this.state.settings, // TODO: apply settings here
       callback: () => {
-        if (typeof callback === 'function') {
+        if (typeof callback === "function") {
           callback();
         }
       }
-    })
+    });
   }
 
   render() {
-
-    const startingSession = get(this.props, `loading['${types.SESSION_START}_${SESSION_TYPE_FLASHCARD}']`);
+    const startingSession = get(
+      this.props,
+      `loading['${types.SESSION_START}_${SESSION_TYPE_FLASHCARD}']`
+    );
 
     return (
       <>
-
         {/* settings dialog */}
         <Dialog
           title="Flashcard Session"
-          isShown={this.state.showSettings}
-          onCloseComplete={() => this.setState({ showSettings: false })}
-          isConfirmDisabled={startingSession}
-          confirmLabel={startingSession ? "Starting..." : "Start"}
-          preventBodyScrolling
-          hasCancel={false}
-          onConfirm={() => this.start(() => {
-            this.setState({ showSettings: false })
-          })}
+          isOpen={this.state.showSettings}
+          onClose={() => this.setState({ showSettings: false })}
         >
           <form
             onSubmit={e => {
               e.preventDefault();
               this.start(() => {
-                this.setState({ showSettings: false })
-              })
+                this.setState({ showSettings: false });
+              });
             }}
           >
-            <FlashcardSettings
-              settings={this.state.settings}
-              onChange={(vals = {}, callback) => {
-                this.setState({
-                  settings: {
-                    ...(get(this.state, 'settings') || {}),
-                    ...vals
-                  }
-                }, callback);
-              }}
-            />
+            <div className={Classes.DIALOG_BODY}>
+              <FlashcardSettings
+                settings={this.state.settings}
+                onChange={(vals = {}, callback) => {
+                  this.setState(
+                    {
+                      settings: {
+                        ...(get(this.state, "settings") || {}),
+                        ...vals
+                      }
+                    },
+                    callback
+                  );
+                }}
+              />
+            </div>
+            <div className={Classes.DIALOG_FOOTER}>
+              <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                <Button type="submit" disabled={startingSession}>
+                  {startingSession && "Starting..."}
+                  {!startingSession && "Start"}
+                </Button>
+              </div>
+            </div>
           </form>
         </Dialog>
 
@@ -94,8 +99,8 @@ class FlashcardLauncher extends React.Component {
           onClick={() => {
             if (startingSession) return;
             this.setState({
-              showSettings: true,
-            })
+              showSettings: true
+            });
           }}
         >
           <H5>Flashcards</H5>
@@ -109,18 +114,15 @@ class FlashcardLauncher extends React.Component {
 FlashcardLauncher.propTypes = {
   loading: PropTypes.bool,
   sessionActions: PropTypes.object,
-  settings: flashcardSettingsPropTypes,
+  settings: flashcardSettingsPropTypes
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  loading: state.loading,
+  loading: state.loading
 });
 
 const mapDispatchToProps = dispatch => ({
-  sessionActions: bindActionCreators(sessionActions, dispatch),
+  sessionActions: bindActionCreators(sessionActions, dispatch)
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(FlashcardLauncher);
+export default connect(mapStateToProps, mapDispatchToProps)(FlashcardLauncher);
